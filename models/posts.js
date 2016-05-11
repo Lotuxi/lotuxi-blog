60cd8e094b4246503e5c1bec534bf5d7ee1b3666
Brittany <lotuxi@me.com>
2016-05-10 22:11:26 -0500

    save before restart

--- models/posts.js
+++ models/posts.js
@@ -1,12 +1,19 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongoosePaginate = require('mongoose-paginate');
 
-var postSchema = new mongoose.Schema({
-    title: { type: String, required: '{PATH} is required!'},
-    body: { type: String, required: '{PATH} is required!'},
+
+var postSchema = new Schema({
+    title: String,
+    body: String,
     created_at: { type: Date, default: Date.now }
 });
 
+postSchema.plugin(mongoosePaginate);
 
-var Posts = mongoose.model('Posts', postSchema);
 
-module.exports = mongoose.model('Post', postSchema);
+
+
+var Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
